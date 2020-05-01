b10e625548435364ef1389ba57fd48784433c779
debanshur <debanshur94@gmail.com>
2020-05-01 23:49:29 +0530

    Add setup.py for setting up user credentials (access_token)

---
+++ setup.py
@@ -0,0 +1,84 @@
+from kiteconnect import KiteConnect
+import pandas as pd
+import os
+import sys
+import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+cur_dir_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+data_path = os.path.join(cur_dir_path, 'data')
+userdata_file = os.path.join(data_path, 'userdata.csv')
+
+def setup():
+    try:
+        if (not os.path.exists(data_path)):
+            os.makedirs(data_path)
+        
+        if (not os.path.exists(userdata_file)):
+            userdata = pd.DataFrame(
+                {
+                    "user_name" : [],
+                    "api_key" : [],
+                    "api_secret" : [],
+                    "access_token" : [],
+                    "token_req_date" : [],
+
+                })
+            api_key = input("Enter api key: ")
+            api_secret = input("Enter api secret: ")
+            userdata.loc[0, 'api_key'] = api_key
+            userdata.loc[0, 'api_secret'] = api_secret
+            userdata.to_csv(userdata_file, index=False)
+    except Exception as e:
+        print("** ERROR in setup. ", e, datetime.datetime.now())
+
+def is_valid_token(api_key, access_token):
+    try:
+        kite = KiteConnect(api_key=api_key)
+        kite.set_access_token(access_token)
+        kite.profile()
+        return True
+    except Exception:
+        return False
+
+
+def generate_token(userdata):
+    try:
+        api_key = userdata.loc[0, 'api_key']
+        api_secret = userdata.loc[0, 'api_secret']
+
+        kite = KiteConnect(api_key=api_key)
+        print("Login URL :=>  " + kite.login_url())
+        request_token = input("Enter new request token value : ")
+        res = kite.generate_session(request_token, api_secret)
+
+        #print(res)
+        userdata.loc[0, 'access_token'] = res['access_token']
+        userdata.loc[0, 'user_name'] = res['user_name']
+        userdata.loc[0, 'token_req_date'] = datetime.datetime.now()
+        userdata.to_csv(userdata_file, index=False)
+    except Exception as e:
+        print("** ERROR in Access Token Generation. ", e, datetime.datetime.now())
+
+
+def validate_access_token():
+    try:
+        userdata = pd.read_csv(userdata_file)
+        api_key = userdata.loc[0, 'api_key']
+        access_token = userdata.loc[0, 'access_token']
+        
+        if not is_valid_token(api_key, access_token):
+            raise Exception("Invalid Token")
+        else :
+            print("Authentication Done")
+
+    except Exception as e:
+        print("** ERROR in Access Token Validation. ", e, datetime.datetime.now())
+        generate_token(userdata)
+        validate_access_token()
+            
+
+setup()
+validate_access_token()
