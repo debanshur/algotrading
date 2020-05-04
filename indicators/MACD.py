da1471fad9307c98e7adb57730560b1f907715a4
debanshur <debanshur94@gmail.com>
2020-05-04 23:14:56 +0530

    Add Indicators (ATR, EMA, MACD, SuperTrend)

---
+++ indicators/MACD.py
@@ -0,0 +1,42 @@
+import numpy as np
+from indicators import EMA
+def calc(df, fastEMA=12, slowEMA=26, signal=9, base='close'):
+    """
+    Function to compute Moving Average Convergence Divergence (MACD)
+    
+    Args :
+        df : Pandas DataFrame which contains ['date', 'open', 'high', 'low', 'close', 'volume'] columns
+        fastEMA : Integer indicates faster EMA
+        slowEMA : Integer indicates slower EMA
+        signal : Integer indicates the signal generator for MACD
+        base : String indicating the column name from which the MACD needs to be computed from (Default Close)
+        
+    Returns :
+        df : Pandas DataFrame with new columns added for 
+            Fast EMA (ema_$fastEMA)
+            Slow EMA (ema_$slowEMA)
+            MACD (macd_$fastEMA_$slowEMA_$signal)
+            MACD Signal (signal_$fastEMA_$slowEMA_$signal)
+            MACD Histogram (MACD (hist_$fastEMA_$slowEMA_$signal)) 
+    """
+
+    fE = "ema_" + str(fastEMA)
+    sE = "ema_" + str(slowEMA)
+    macd = "macd_" + str(fastEMA) + "_" + str(slowEMA) + "_" + str(signal)
+    sig = "signal_" + str(fastEMA) + "_" + str(slowEMA) + "_" + str(signal)
+    hist = "hist_" + str(fastEMA) + "_" + str(slowEMA) + "_" + str(signal)
+
+    # Compute fast and slow EMA    
+    EMA.calc(df, base, fE, fastEMA)
+    EMA.calc(df, base, sE, slowEMA)
+    
+    # Compute MACD
+    df[macd] = np.where(np.logical_and(np.logical_not(df[fE] == 0), np.logical_not(df[sE] == 0)), df[fE] - df[sE], 0)
+    
+    # Compute MACD Signal
+    EMA.calc(df, macd, sig, signal)
+    
+    # Compute MACD Histogram
+    df[hist] = np.where(np.logical_and(np.logical_not(df[macd] == 0), np.logical_not(df[sig] == 0)), df[macd] - df[sig], 0)
+    
+    return df
