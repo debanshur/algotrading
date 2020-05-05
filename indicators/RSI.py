7a7c2db906be25d92217a1488c34c26558e1f640
Debanshu Rout <debanshur94@gmail.com>
2020-05-06 02:13:31 +0530

    Add strategy (#9)
    Add RSI Indicator

---
+++ indicators/RSI.py
@@ -0,0 +1,27 @@
+def calc(df, base="close", period=14):
+    """
+    Function to compute Relative Strength Index (RSI)
+    
+    Args :
+        df : Pandas DataFrame which contains ['date', 'open', 'high', 'low', 'close', 'volume'] columns
+        base : String indicating the column name from which the RSI needs to be computed from (Default Close)
+        period : Integer indicates the period of computation in terms of number of candles
+        
+    Returns :
+        df : Pandas DataFrame with new columns added for 
+            Relative Strength Index (RSI_$period)
+    """
+ 
+    delta = df[base].diff()
+    up, down = delta.copy(), delta.copy()
+
+    up[up < 0] = 0
+    down[down > 0] = 0
+    
+    rUp = up.ewm(com=period - 1,  adjust=False).mean()
+    rDown = down.ewm(com=period - 1, adjust=False).mean().abs()
+
+    df['RSI_' + str(period)] = 100 - 100 / (1 + rUp / rDown)
+    df['RSI_' + str(period)].fillna(0, inplace=True)
+
+    return df
